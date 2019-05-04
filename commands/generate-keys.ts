8db7993834ddb517c889c1eb8b88327eee645a04
Sora Suegami <suegamisora@gmail.com>
2019-05-04 17:31:47 +0900

    fix bugs in generate-keys

--- commands/generate-keys.ts
+++ commands/generate-keys.ts
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync'
 import * as fs from 'fs'
 import {promisify} from 'util'
 import CryptoJS from 'crypto-js'
-import {new_obj} from '../logic/work';
 
 
 export default async ()=>{
@@ -14,15 +13,6 @@ export default async ()=>{
     const private_key = vr.crypto.genereate_key();
     const public_key = vr.crypto.private2public(private_key);
     const encrypted_pri = CryptoJS.AES.encrypt(private_key,my_key).toString();
-    const config = JSON.parse(await promisify(fs.readFile)('./config/config.json','utf-8'));
-    const new_config = new_obj(
-        config,
-        con=>{
-            con.pub_keys.push(public_key);
-            return con;
-        }
-    );
     await promisify(fs.writeFile)('./keys/private/'+my_key+'.txt',encrypted_pri);
     await promisify(fs.writeFile)('./keys/public/'+my_key+'.txt',public_key);
-    await promisify(fs.writeFile)('./config/config.json',JSON.stringify(new_config,null,4),'utf-8');
 }
