b69de0f55b62d9ae20dde4ad1141eaf30b89af50
Sora Suegami <31360991+SoraSuegami@users.noreply.github.com>
2019-05-18 02:16:04 +0900

    Delete share_data.js

--- share/share_data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data = {
-    chain: [],
-    pool: {}
-};
-exports.default = data;
-//# sourceMappingURL=share_data.js.map
